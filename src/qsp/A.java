37503f76d1f8cd4f87f24c7f9bb70863a7590b31
Shahrukh1207 <43536867+Shahrukh1207@users.noreply.github.com>
2018-09-25 11:35:34 +0530

    Update A.java

--- src/qsp/A.java
+++ src/qsp/A.java
@@ -1,5 +1,15 @@
 package qsp;
 
 public class A {
+import java.io.File;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+public class A {
+	public static void main(String [] args) throws Exception {
+		Workbook w = WorkbookFactory.create(new File("./Book1.xlxs"));
+		String s = w.getSheet("Sheet1").getRow(0).getCell(0).toString();
+		System.out.println(s);
+	}
 }
